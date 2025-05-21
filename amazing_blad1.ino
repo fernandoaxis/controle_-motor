f86956479ce52dcfc0e0f5d97b73b8d89ea278e3
Fernando S. Axis <159575977+fernandoaxis@users.noreply.github.com>
2025-05-21 20:27:45 -0300

    Update amazing_blad1.ino

--- amazing_blad1.ino
+++ amazing_blad1.ino
@@ -6,6 +6,7 @@ int BOT2 = 10;
 int BOT3 = 8;
 int LED1 = 13;
 int LED2 = 12;
+int LED3 = 2;
 void setup()
 {
 	pinMode(IN1, OUTPUT);
@@ -15,9 +16,11 @@ void setup()
 	pinMode(BOT3, INPUT_PULLUP);
 	pinMode(LED1, OUTPUT);
 	pinMode(LED2, OUTPUT);
+  	pinMode(LED3, OUTPUT);
 }
 
 void loop()
+  //CONFIGURAÇÃO BOTÕES
 {
   
   if(digitalRead(BOT1) == LOW){
@@ -30,21 +33,28 @@ void loop()
 	analogWrite(EN, 70);
   }else if(digitalRead(BOT3) == LOW){
 	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, LOW);
+	digitalWrite(IN2, LOW);    
   }
   
-  //-------------------------------------------------------//
+  // CONFIGURAÇÕES DOS LEDS
   
   if(digitalRead(IN1) == HIGH && digitalRead(IN2) == LOW){
 	digitalWrite(LED1, HIGH);
 	digitalWrite(LED2, LOW);
+   	digitalWrite(LED3, LOW);
   }else if(digitalRead(IN2) == HIGH && digitalRead(IN1) == LOW){
 	digitalWrite(LED2, HIGH);
 	digitalWrite(LED1, LOW);
-  }else if(digitalRead(BOT3) == LOW)
+   	digitalWrite(LED3, LOW);
+  }else if(digitalRead(BOT3) == LOW){
 	digitalWrite(LED1, LOW);
 	digitalWrite(LED2, LOW);
   }
+
+//CONFIGURAÇÃO LED MOTOR DESLIGADO
+  if(analogRead(EN) == 0){
+   	digitalWrite(LED3, HIGH);
+  }
   
-  
-  
+
+}
